02bb7462f5210a0d1bc10ff1532184426355c60c
graham endean <grahamendean@grahams-mbp.lan>
2020-07-03 11:32:48 -0400

    update

--- Instructions/PyPoll/maintwo.py
+++ Instructions/PyPoll/maintwo.py
@@ -1,3 +1,23 @@
 import os
 import csv
 
+#upload csv
+file_to_load = os.path.join("Resources/election_data.csv")
+output = ""
+
+with open (file_to_load) as csvfile:
+    csvreader = csv.reader(csvfile)
+
+    headers = next(csvreader)
+    first_row = next(csvreader)
+
+total_votes = 0
+candidates_list = 0
+percentage_of_winner = 0
+total_winner = int(first_row[1])
+popular_vote = int(first_row[1])
+
+
+for row in csvreader:
+    total_votes += 1
+    candidates_list += int(row[1])
